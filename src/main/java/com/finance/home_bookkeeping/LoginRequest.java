cc2730aaf8555994a6a5f77a8c554babc6325944
ValeriCuk <pakutasieva@gmail.com>
2025-03-27 13:36:25 +0200

    first commit

---
+++ src/main/java/com/finance/home_bookkeeping/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.finance.home_bookkeeping;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String username;
+    private String password;
+}
---
+++ src/test/java/com/finance/home_bookkeeping/HomeBookkeepingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.home_bookkeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBookkeepingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
